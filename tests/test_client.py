a366029f7704eda691d7f42c6a49726800234ad7
Gjum <code.gjum@gmail.com>
2015-10-02 21:53:19 +0200

    Add tests to check that server behaviour hasn't changed

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,4 +1,5 @@
-from unittest import TestCase
+import select
+import unittest
 
 from agarnet.client import Client
 from agarnet.utils import find_server
@@ -9,13 +10,13 @@ class SubscriberMock(object):
         self.events = []
         self.data = []
 
+    def reset(self):
+        self.events.clear()
+        self.data.clear()
+
     def __getattr__(self, item):
-        assert item[:3] == 'on_', \
-            'Requested non-event handler from subscriber'
-        assert item not in (
-            'on_connect_error',
-            'on_sock_error',
-        ), 'Error event emitted'
+        assert item[:3] == 'on_', 'Requested non-event handler from subscriber'
+        assert 'error' not in item, 'Error event emitted'
         event = item[3:]
         data = {}
         self.events.append(event)
@@ -23,7 +24,7 @@ class SubscriberMock(object):
         return lambda **d: data.update(d)
 
 
-class ClientTest(TestCase):
+class ClientTest(unittest.TestCase):
     def test_connect(self):
         nick = 'Dummy'
         address, token = find_server()
@@ -40,7 +41,7 @@ class ClientTest(TestCase):
         try:
             success = client.connect(address, token)
         except ConnectionResetError:
-            # sometimes connection gets closed on first attempt
+            print('Connection got closed on first attempt, retrying')
             success = client.connect(address, token)
 
         self.assertTrue(success)
@@ -48,7 +49,89 @@ class ClientTest(TestCase):
         self.assertEqual(address, client.address)
         self.assertEqual(token, client.server_token)
         self.assertEqual(nick, client.player.nick)
-        self.assertEqual('sock_open', subscriber.events[-1])
+        self.assertEqual(1, len(subscriber.events))
+        self.assertEqual('sock_open', subscriber.events[0])
+
+        # the following tests cover expected server behavior and responses
+
+        def poll_select(client):
+            """
+            Waits until the client receives a packet
+            then calls its on_message() method to receive one packet.
+            """
+            r = False
+            while not r:
+                r, w, e = select.select((client.ws.sock,), (), ())
+                assert not e, 'Error while receiving from socket'
+            client.on_message()
+
+        def pop_and_check_keys(event, *keys):
+            received_event = subscriber.events.pop(0)
+            received_data = subscriber.data.pop(0)
+            self.assertEqual(event, received_event)
+            if keys:
+                self.assertSetEqual(set(keys), set(received_data.keys()))
+            else:
+                self.assertDictEqual({}, received_data)
+
+        # receive world_rect packet
+        subscriber.reset()
+        poll_select(client)
+        pop_and_check_keys('ingame')
+        pop_and_check_keys('world_rect', 'left', 'top', 'right', 'bottom')
+        if len(subscriber.events) > 2:  # optional, might be removed later
+            pop_and_check_keys('server_version', 'number', 'text')
+
+        # receive at least one leaderboard and world_update
+        received = {
+            'leaderboard': False, 'world_update': False,
+            'cell_info': False, 'cell_eaten': False,
+            'cell_removed': True,  # not required, but checked
+        }
+        for i in range(30):  # assuming <30tps and >1 leaderboard per second
+            # receive any packet
+            subscriber.reset()
+            poll_select(client)
+            if subscriber.events[0] == 'leaderboard_names':
+                received['leaderboard'] = True
+                pop_and_check_keys('leaderboard_names', 'leaderboard')
+            elif subscriber.events[0] == 'world_update_pre':
+                received['world_update'] = True
+                self.assertEqual('world_update_post', subscriber.events[-1])
+                self.assertDictEqual({}, subscriber.data[0])
+                self.assertDictEqual({}, subscriber.data[-1])
+                for evt, data in zip(subscriber.events, subscriber.data):
+                    if evt == 'cell_info':
+                        received['cell_info'] = True
+                        self.assertSetEqual({'cid', 'x', 'y', 'size', 'name',
+                                             'color', 'is_virus', 'is_agitated'
+                                             }, set(data.keys()))
+                    elif evt == 'cell_eaten':
+                        received['cell_eaten'] = True
+                        self.assertSetEqual({'eater_id', 'eaten_id'},
+                                            set(data.keys()))
+                    elif evt == 'cell_removed':
+                        received['cell_removed'] = True
+                        self.assertSetEqual({'cid'}, set(data.keys()))
+                    elif evt not in ('world_update_pre', 'world_update_post'):
+                        self.fail('Got unexpected event during world_update:'
+                                  ' %s %s' % (evt, data))
+            else:
+                self.fail('Unexpected event %s %s'
+                          % (subscriber.events, subscriber.data))
+            if all(received.values()):
+                break  # we received all packets we want to check
+        else:
+            self.fail('Did not receive all wanted packets, missing: %s'
+                      % ', '.join(e for e, v in received.items() if not v))
+
+        # TODO test spectating
+
+        # TODO test movement
+
+        # TODO test split/shoot
+
+        # do not test respawn, we might not get eaten for a long time
 
         client.disconnect()
 
@@ -58,3 +141,6 @@ class ClientTest(TestCase):
         self.assertEqual(token, client.server_token)
         self.assertEqual(nick, client.player.nick)
         self.assertEqual('sock_closed', subscriber.events[-1])
+
+if __name__ == '__main__':
+    unittest.main()
