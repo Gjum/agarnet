01c60b299dd27b45583103eb36f2890ccb30fddd
Gjum <code.gjum@gmail.com>
2015-12-24 22:47:31 +0100

    Remove unnecessary skin url check (should be done by subscriber)

--- agarnet/client.py
+++ agarnet/client.py
@@ -202,8 +202,6 @@ class Client(object):
                     buf.pop_uint8()
             if bitmask & 4:  # skin URL
                 skin_url = buf.pop_str8()
-                if skin_url[0] is not ':':
-                    skin_url = ''
             else:  # no skin URL given
                 skin_url = ''
 
