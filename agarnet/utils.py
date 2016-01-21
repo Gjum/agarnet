cce15c5ddca955090597142e118d13d9c1df5ea2
Gjum <code.gjum@gmail.com>
2016-01-21 23:24:20 +0100

    Rename utils.moz_headers to default_headers

--- agarnet/utils.py
+++ agarnet/utils.py
@@ -7,8 +7,9 @@ from .client import handshake_version
 special_names = 'poland;usa;china;russia;canada;australia;spain;brazil;germany;ukraine;france;sweden;chaplin;north korea;south korea;japan;united kingdom;earth;greece;latvia;lithuania;estonia;finland;norway;cia;maldivas;austria;nigeria;reddit;yaranaika;confederate;9gag;indiana;4chan;italy;bulgaria;tumblr;2ch.hk;hong kong;portugal;jamaica;german empire;mexico;sanik;switzerland;croatia;chile;indonesia;bangladesh;thailand;iran;iraq;peru;moon;botswana;bosnia;netherlands;european union;taiwan;pakistan;hungary;satanist;qing dynasty;matriarchy;patriarchy;feminism;ireland;texas;facepunch;prodota;cambodia;steam;piccolo;ea;india;kc;denmark;quebec;ayy lmao;sealand;bait;tsarist russia;origin;vinesauce;stalin;belgium;luxembourg;stussy;prussia;8ch;argentina;scotland;sir;romania;belarus;wojak;doge;nasa;byzantium;imperial japan;french kingdom;somalia;turkey;mars;pokerface;8;irs;receita federal;facebook;putin;merkel;tsipras;obama;kim jong-un;dilma;hollande;berlusconi;cameron;clinton;hillary;venezuela;blatter;chavez;cuba;fidel;palin;queen;boris;bush;trump' \
     .split(';')
 
-
-moz_headers = [
+# Used to make the requests to `m.agar.io`. Sets the
+# `User-Agent` to Firefox and the `Origin` and `Referer` to `http://agar.io`.
+default_headers = [
     ('User-Agent',
         'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0'),
     ('Origin', 'http://agar.io'),
@@ -30,7 +31,7 @@ def find_server(region='EU-London', mode=None):
     if mode:
         region = '%s:%s' % (region, mode)
     opener = urllib.request.build_opener()
-    opener.addheaders = moz_headers
+    opener.addheaders = default_headers
     data = '%s\n%s' % (region, handshake_version)
     return opener.open('http://m.agar.io/', data=data.encode()) \
         .read().decode().split('\n')[0:2]
@@ -49,7 +50,7 @@ def get_party_address(party_token):
     Makes a request to http://m.agar.io/getToken to get the address.
     """
     opener = urllib.request.build_opener()
-    opener.addheaders = moz_headers
+    opener.addheaders = default_headers
     try:
         return opener.open('http://m.agar.io/getToken', data=party_token.encode()) \
             .read().decode().split('\n')[0]
